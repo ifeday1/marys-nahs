34a5db608d51695efe11ae61af5f09dce94d2d0a
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2024-08-12 10:15:54 +0100

    Project setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ['./src/**/*.{html,js,jsx}'],
+  content: ['./src/**/*.{html,js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        // gold: '#ae7918',
+        // dark: '#704e11',
+      },
+    },
+  },
+  plugins: [],
+};
